5652024a367a3667fa9f7b914f0fb0563a52c8af
manikantabhavana <manikanta.7828@gmail.com>
2024-02-22 17:06:45 +0530

    user model added

--- Models/User.js
+++ Models/User.js
@@ -1,1 +1,56 @@
-const express=require("express");
+const {Sequelize,DataTypes}=require('sequelize')
+const config=require('../Config/Config')
+const sequelize=new Sequelize(config.development)
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+  const User = sequelize.define('User', {
+
+    username: { 
+      type: DataTypes.STRING, 
+      allowNull: true,
+      
+  },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+   
+    Name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+       
+    },
+    Age:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+
+    },
+    Gender: {
+        type: DataTypes.ENUM('Male', 'Female'),
+      allowNull: false,
+     
+      },
+      Gender: {
+        type: DataTypes.ENUM('Active', 'Inactive'),
+      allowNull: false,
+      defaultValue: 'Active',
+     
+      },
+    
+    
+   
+  }, {
+      timestamps:true,
+   
+    tableName: 'users'
+   
+  });
+
+  module.exports = { User, sequelize };
+
